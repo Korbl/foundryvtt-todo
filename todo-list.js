2faf368f6ba07cc51606b7f353aa34cb602f7edb
Seth Itschner <sitschner@schubergphilis.com>
2021-11-18 17:07:57 +0100

    moving to typescript

--- todo-list.js
+++ todo-list.js
@@ -1,1 +1,5 @@
 console.log('todo-list | Hellow world!');
+
+// https://github.com/League-of-Foundry-Developers/FoundryVTT-Module-Template/blob/master/.github/workflows/main.yml
+
+// https://github.com/League-of-Foundry-Developers/foundry-typescript-template
